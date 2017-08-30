a7b4451bef2924b54591390f64dfbbe6eaaa34c6
Ondrej Chvala <ondrejch@gmail.com>
2017-08-30 17:03:32 -0400

    check address if no description

--- ListBannedIPs.pl
+++ ListBannedIPs.pl
@@ -3,17 +3,21 @@
 # Ondrej Chvala, ondrejch@gmail.com
 
 # Whois field of interest. NetName is another useful one.
-$myfield = 'descr:';
+$myfield = 'descr';
 
 # Get list of banned IPs
 @iplist =`sudo iptables -L sshguard -n | awk '/DROP/{print \$4;}'`;
 
 # Whois lookup and print results
-$i=0;
+$i = 1;     # Index starts from 1 so it corresponds to iptables chain #
 foreach $ip (@iplist) {
 	chomp $ip;
-	$whodata = `whois $ip | grep -i $myfield | head -n1 | sed -e s/$myfield//g`;
+	$whodata = `whois $ip | grep -i $myfield | head -n1 | sed -e s/${myfield}:\\ */\\ \\ /g`;
     chomp $whodata;
+    if ($whodata eq "") {   # Nothin in $myfield, use address
+        $whodata = `whois $ip | grep -i 'Address\\|City\\|state'  -m3 | sed -e s/.*:\\ *//g | sed -e ':a;N;\$!ba;s/\\n/, /g'`;
+        chomp $whodata;
+    }
 	print $i++,"\t", $ip,"\t",$whodata,"\n";
 }
 
